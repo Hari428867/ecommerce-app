5524af0b894fbf65abfda50475c759a90c870720
Hari428867 <harishgottapu243@gmail.com>
2025-02-13 11:09:20 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-gray-900 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Your Store</h1>
+        <h1 className="text-2xl font-bold">Mini Kart</h1>
         <div>
           <Link to="/" className="mx-4 hover:text-gray-400">Home</Link>
           <Link to="/shop" className="mx-4 hover:text-gray-400">Shop</Link>

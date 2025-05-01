585eb78d2f7d5f1d255019510ef42f65a31c29fe
Hari428867 <harishgottapu243@gmail.com>
2025-05-01 22:45:08 +0530

    Update Laptops.jsx

--- src/pages/categories/Laptops.jsx
+++ src/pages/categories/Laptops.jsx
@@ -10,16 +10,17 @@ function Laptops() {
         {/* Laptop Accessory 1 */}
         <div className="p-4 border rounded-lg shadow-md text-center hover:shadow-lg transition-shadow">
           <img
-            src="https://m.media-amazon.com/images/I/61I7gGpNquL._SL1500_.jpg"
-            alt="Laptop Stand"
+            src="/images/hp13thgen"
+            alt="hp 13th gen"
             className="w-full h-48 object-cover mb-2"
           />
-          <h3 className="font-semibold text-lg">Adjustable Aluminum Laptop Stand</h3>
-          <p className="text-red-600 font-bold text-xl mt-1">₹999</p>
-          <p className="text-gray-500 line-through text-sm">₹1,599</p>
-          <span className="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-full mt-1">-38%</span>
+          <h3 className="font-semibold text-lg">HP 15, 13th Gen Intel Core i5-1334U (16GB DDR4, 512GB SSD) Microsoft365* Office2024, Win11, 15.6inch(39.6cm) FHD Laptop, Intel Iris Xe, FHD Camera w/Privacy Shutter, Backlit, Silver, 1.59kg, fd0467tu</h3>
+          <p className="text-red-600 font-bold text-xl mt-1">₹55,990</p>
+          <p className="text-gray-500 line-through text-sm">₹65,387</p>
+          <span className="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-full mt-1">-14%</span>
+          <span className="inline-block bg-red-500 text-white text-xs px-2 py-1 rounded-full mt-1">Great Summer Sale</span>
           <a
-            href="https://amzn.to/your-affiliate-link"
+            href="https://amzn.to/4iGupi0"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-3 inline-block bg-yellow-500 hover:bg-yellow-600 text-white font-semibold px-4 py-2 rounded-full transition-transform hover:scale-105"
